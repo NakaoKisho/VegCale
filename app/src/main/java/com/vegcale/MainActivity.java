a5345c721ac132d0d363dbb9d6509d37383a77e5
kisho <nk.110181@icloud.com>
2023-01-09 21:22:30 +0900

    update change navigation
    change from tab to bottom navigation

--- app/src/main/java/com/vegcale/MainActivity.java
+++ app/src/main/java/com/vegcale/MainActivity.java
@@ -10,23 +10,14 @@
 package com.vegcale;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -39,57 +30,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int bottomNavigationButtonId = item.getItemId();
-//                System.out.println(findViewById(R.id.bottom_navigation_action_1).getClass());
-//                Menu bottomNavigationButtonMenu = bottomNavigationView.getMenu();
-//                getMenuInflater().inflate(R.menu.home_bottom_navigation, bottomNavigationButtonMenu);
-//                MenuItem action1MenuItem = findViewById(R.id.bottom_navigation_action_1);
-//                MenuItem action2MenuItem = findViewById(R.id.bottom_navigation_action_2);
-//                MenuItem action3MenuItem = findViewById(R.id.bottom_navigation_action_3);
-                NavDirections action = null;
 
-                if (bottomNavigationButtonId == R.id.bottom_navigation_action_1) {
-                    action = HomeFragmentDirections.actionHomeFragmentToCalendarFragment();
-                } else if (bottomNavigationButtonId == R.id.bottom_navigation_action_2) {
-                    action = HomeFragmentDirections.actionHomeFragmentToCalendarFragment();
-                } else if (bottomNavigationButtonId == R.id.bottom_navigation_action_3) {
-                    action = HomeFragmentDirections.actionHomeFragmentToTipFragment();
-                }
-//                if (bottomNavigationButtonId == action1MenuItem.getItemId()) {
-//                    action = HomeFragmentDirections.actionHomeFragmentToCalendarFragment();
-//                } else if (bottomNavigationButtonId == action2MenuItem.getItemId()) {
-//                    action = HomeFragmentDirections.actionHomeFragmentToCalendarFragment();
-//                } else if (bottomNavigationButtonId == action3MenuItem.getItemId()) {
-//                    action = HomeFragmentDirections.actionHomeFragmentToTipFragment();
-//                }
-
-                if (action == null) {
-                    throw new NullPointerException();
-                }
-
-                NavController navController = test();
-                navController.navigate(action);
-
-                return false;
-            }
-        });
+        bottomNavigationView.setOnItemSelectedListener(getBottomNavigationOnItemSelectedListener());
     }
 
     private void initializeGoogleMobileAdsSdk() {
         MobileAds.initialize(this, initializationStatus -> { });
     }
 
-    private NavController test() {
-        NavHostFragment navigationContainer =
-                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_container);
+    private NavigationBarView.OnItemSelectedListener getBottomNavigationOnItemSelectedListener() {
+        return item -> {
+            int bottomNavigationButtonId = item.getItemId();
+            changeFragment(bottomNavigationButtonId);
 
-        if (navigationContainer == null) {
+            return true;
+        };
+    }
+
+    private void changeFragment(int bottomNavigationButtonId) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setReorderingAllowed(true);
+        Fragment directionFragment = null;
+
+        if (bottomNavigationButtonId == R.id.bottom_navigation_action_1) {
+            directionFragment = new HomeFragment();
+        } else if (bottomNavigationButtonId == R.id.bottom_navigation_action_2) {
+            directionFragment = new CalendarFragment();
+        } else if (bottomNavigationButtonId == R.id.bottom_navigation_action_3) {
+            directionFragment = new TipFragment();
+        }
+
+        if (directionFragment == null) {
             throw new NullPointerException();
         }
 
-        return navigationContainer.getNavController();
+        fragmentTransaction.replace(R.id.navigation_container, directionFragment);
+        fragmentTransaction.commit();
     }
 }
