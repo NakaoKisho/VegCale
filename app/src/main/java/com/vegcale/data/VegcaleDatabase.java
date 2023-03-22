ce5a8c44ee58c0db136c99bc5bcba74ba9e83409
haiji0504 <55823720+haiji0504@users.noreply.github.com>
2023-03-22 21:33:54 +0900

    Update VegcaleDatabase.java

--- app/src/main/java/com/vegcale/data/VegcaleDatabase.java
+++ app/src/main/java/com/vegcale/data/VegcaleDatabase.java
@@ -26,7 +26,7 @@ public class VegcaleDatabase {
 
     public VegcaleDatabase() {
         final String vegcaleDatabaseUrl =
-                "https://vegcale-app-default-rtdb.asia-southeast1.firebasedatabase.app/";
+                "url to firebase/";
 
         vegcaleDatabase = FirebaseDatabase.getInstance(vegcaleDatabaseUrl);
     }
